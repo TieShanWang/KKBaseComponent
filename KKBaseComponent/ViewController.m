f99f993ce12085f04f5abc4e9d0f02950bd7a391
KK <king@KING-KK.local>
2016-10-26 14:31:43 +0800

    create

---
+++ KKBaseComponent/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KKBaseComponent
+//
+//  Created by KING on 2016/10/7.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import "KKBaseComponent/KKBaseComponent.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
